3e373d94e108bfd4b6ad3ad80091f4b9cf066ed4
labcam-ufop <labcam.ufop@gmail.com>
2021-11-11 11:01:10 -0300

    feat: adicionei supervisorio

---
+++ Supervisorio/Serial/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Serial
+{
+    public partial class Form1 : Form
+    {
+
+        //VARIÁVEIS
+        string InputData = String.Empty;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            //obter as portas COM disponíveis
+            String[] ports = SerialPort.GetPortNames();
+            // adicionar as portas ao combobox
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+            serialPort1.PortName = comboBox1.SelectedItem.ToString();
+
+            // try to open the selected port:
+            try
+            {
+                serialPort1.Open();
+            }
+            // give a message, if the port is not available:
+            catch
+            {
+                MessageBox.Show("A porta serial " + serialPort1.PortName +
+                   " não pode ser aberta", "Erro",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                comboBox1.SelectedText = "";
+            }
+        }
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.WriteLine(textBoxSaida.Text);                
+            }
+            else MessageBox.Show("A porta serial está fechada!",
+                                 "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            textBoxSaida.Clear();
+
+        }
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxEntrada.Clear();
+        }
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //RECEBE STRING
+            InputData = serialPort1.ReadLine(); 
+            this.Invoke(new EventHandler(DisplayText));
+        }
+        private void DisplayText(object sender, EventArgs e)
+        {
+            string input, adc1;
+            int i = 0;
+            //textBoxEntrada.Clear();
+            textBoxEntrada.AppendText(InputData);
+            input = InputData.ToString();
+            try
+            {
+                // Trata dados da string recebida /*0000/
+                if (input != string.Empty)
+                {
+                    i = input.IndexOf("*"); //Obtem índice onde aparece *
+                    adc1 = input.Substring(i + 1, 4); // captura 4 bytes após * (0000)
+                    label1.Text = adc1; // passa valor para o label1
+                    aChart1.Series["Temperatura"].Points.AddY(adc1); // escreve no gráfico
+                                                                     // Escreve na tabela do DataGridView
+                    var row = new DataGridViewRow();
+                    row.CreateCells(dataGridViewSensor);
+                    row.Cells[0].Value = adc1;
+                    row.Cells[1].Value = "variável 2";
+                    row.Cells[2].Value = "variável 3";
+                    row.Cells[3].Value = "variável 4";
+                    row.Cells[4].Value = "variável 5";
+                    row.Cells[5].Value = "variável 6";
+                    dataGridViewSensor.Rows.Add(row);
+                }
+            }
+            catch { }
+        }
+
+        private void btAtualizar_Click(object sender, EventArgs e)
+        {
+            //obter as portas COM disponíveis
+            String[] ports = SerialPort.GetPortNames();
+            // adicionar as portas ao combobox
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            this.Close();
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form sobre = new Form2();
+            sobre.Show();
+        }
+
+        private void btLiga_Click(object sender, EventArgs e)
+        {
+
+            serialPort1.WriteLine("liga");
+        }
+
+        private void btdesliga_Click(object sender, EventArgs e)
+        {
+            serialPort1.WriteLine("d");
+        }
+    }
+}
---
+++ Supervisorio/Serial/Form2.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Serial
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(283, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(228, 247);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "UNIVERSIDADE FEDERAL DE OURO PRETO\nESCOLA DE MINAS\nDECAT\n\nExemplo de como realiza" +
+                "r comunicação serial e armazenamento de dados em um banco de dados.\nProfessor Al" +
+                "an Kardek.\nemail: alankardek@em.ufop.br";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::Serial.Properties.Resources.escola_de_minas;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(265, 247);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 275);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form2";
+            this.Text = "Sobre";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
